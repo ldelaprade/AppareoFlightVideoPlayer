4ef693c3720ff16620538b3e29cdda840de7058d
laurent de Laprade <ldelaprade@appareo.com>
2025-06-13 18:22:26 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace AppareoFlightVideoPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+        private DispatcherTimer _timer;
+        private bool _isDraggingSeekBar = false;
+        private bool _isFullscreen = false;
+        private WindowState _previousWindowState;
+        private WindowStyle _previousWindowStyle;
+        private ResizeMode _previousResizeMode;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Path to the VLC native libraries
+            var libVlcPath = Path.Combine(AppContext.BaseDirectory, "libvlc", "win-x64");
+
+            // Initialize LibVLC with the path to native libraries
+            Core.Initialize(libVlcPath);
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+            videoView.MediaPlayer = _mediaPlayer;
+
+            _mediaPlayer.LengthChanged += MediaPlayer_LengthChanged;
+            _mediaPlayer.TimeChanged += MediaPlayer_TimeChanged;
+
+            VolumeSlider.Value = _mediaPlayer.Volume;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(500);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Video Files|*.mp4;*.avi;*.mkv;*.mov;*.wmv;*.flv;*.ts|All Files|*.*"
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                _mediaPlayer.Stop();
+                using var media = new Media(_libVLC, dialog.FileName, FromType.FromPath);
+                _mediaPlayer.Play(media);
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_mediaPlayer.Media != null)
+                _mediaPlayer.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_mediaPlayer.CanPause)
+                _mediaPlayer.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Stop();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_mediaPlayer != null)
+                _mediaPlayer.Volume = (int)VolumeSlider.Value;
+        }
+
+        private void MediaPlayer_LengthChanged(object sender, MediaPlayerLengthChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                SeekBar.Maximum = e.Length;
+            });
+        }
+
+        private void MediaPlayer_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (!_isDraggingSeekBar)
+                {
+                    SeekBar.Value = e.Time;
+                }
+                UpdateTimeText();
+            });
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (_mediaPlayer.Media != null && !_isDraggingSeekBar)
+            {
+                SeekBar.Value = _mediaPlayer.Time;
+                UpdateTimeText();
+            }
+        }
+
+        private void SeekBar_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDraggingSeekBar = true;
+        }
+
+        private void SeekBar_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_mediaPlayer.Media != null)
+            {
+                _mediaPlayer.Time = (long)SeekBar.Value;
+            }
+            _isDraggingSeekBar = false;
+        }
+
+        private void FullscreenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isFullscreen)
+            {
+                _previousWindowState = WindowState;
+                _previousWindowStyle = WindowStyle;
+                _previousResizeMode = ResizeMode;
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Maximized;
+                ResizeMode = ResizeMode.NoResize;
+                _isFullscreen = true;
+            }
+            else
+            {
+                WindowStyle = _previousWindowStyle;
+                WindowState = _previousWindowState;
+                ResizeMode = _previousResizeMode;
+                _isFullscreen = false;
+            }
+        }
+
+        private void UpdateTimeText()
+        {
+            var current = TimeSpan.FromMilliseconds(_mediaPlayer.Time);
+            var total = TimeSpan.FromMilliseconds(_mediaPlayer.Length);
+            TimeText.Text = $"{current:hh\\:mm\\:ss} / {total:hh\\:mm\\:ss}";
+        }
+    }
+}
